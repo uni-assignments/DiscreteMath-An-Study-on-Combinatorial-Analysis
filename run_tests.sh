b16de1116542bff8638f10d67390ef10167b05cc
RitaRez <ritarezborges@gmail.com>
2021-01-04 15:11:14 -0300

    added gitignore

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 EXEC=$1
 TMP_OUT=$2
 
-for i in {0..2..1}; do
+for i in {0..7..1}; do
   testname=$(printf "%01d" $i)
   $EXEC < ./../tests/$testname.in > $TMP_OUT
   if ! diff -qwB ./../tests/$testname.out $TMP_OUT &>/dev/null; then
