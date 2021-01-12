32402692458679ea58eac94509da0a64c8c5adf9
RitaRez <ritarezborges@gmail.com>
2021-01-12 16:07:07 -0300

    made greedy algorithm

--- ceasars_legions.c
+++ ceasars_legions.c
@@ -54,15 +54,34 @@ char *greedy_response(int n1, int n2, int k1, int k2){
     char *resp = (char*)malloc(size*sizeof(char));
     
     for (size_t i = 0; i < size; i++){
-        if(n1 < n2 && aux1 > 0){
-            resp[i] = 'S';
-            aux2 = k2;
-            aux1--;
+        if(n1 == 0 && n2 == 0){
+            resp[i] = '*';
+            break;
         }
-        else {
-            resp[i] = 'C';
-            aux1 = k1;
-            aux2--;
+        if(n1 >= n2){
+            if(aux1 > 0){
+                n1--;
+                aux1--;
+                resp[i] = 'S';
+                aux2 = k2;
+            } else {
+                n2--;
+                aux2--;
+                resp[i] = 'H';
+                aux1 = k1;
+            }    
+        } else {
+            if(aux2 > 0){
+                n2--;
+                aux2--;
+                resp[i] = 'H';
+                aux1 = k1;
+            } else {
+                n1--;
+                aux1--;
+                resp[i] = 'S';
+                aux2 = k2;
+            }
         }
     }
     return resp;
@@ -72,4 +91,11 @@ int main(){
     scanf("%d %d %d %d", &n1, &n2, &k1, &k2);
     memo = generate_memoization_matrix(n1 + 1, n2 + 1, k1 + 1, k2 + 1);
     printf("Quantidade de maneiras de colocar uma sequencia: %d\n", ceasars_legions(n1, n2, 0, 0));
+    char *resp = greedy_response(n1,n2, k1, k2);
+    if(resp[n1+n2-1] != '*'){
+        for (size_t i = 0; i < n1+n2; i++){
+            printf("%c", resp[i]);
+        }
+    }
+    printf("\n");
 }
